28f0c07b71e6ea5f48f89204ac714200fb94ec96
Halaubi <hmlaubinger1s@semo.edu>
2020-07-03 20:52:12 -0500

    Finished Part 1

---
+++ Hw02/hw02-1.R
@@ -0,0 +1,29 @@
+# Hayley Laubinger
+# HW02 Part 1: Intro to Basics
+# Calculate 2 + 5
+### Section 1.2
+# Division
+# Exponentiation
+#Modulo
+# Exponentiation without parentheses
+# Exponentiation with parentheses
+### Section 1.3
+# Assign the value 42 to x
+# Print out the value of the variable x
+# Assign the value 24 the variable d_merriami
+# Assign the value 9 to d_ordii
+# Check the results
+### 1.4 Working with variables
+# Correcting the mistake
+# Add together the number of individuals of each species. 
+# Store the result in 'total_k_rats'.
+# Assign 1000 to a variable called 'area_sampled'
+# Divide 'total_k_rats' by 'area_sampled' to calculate 'density'. 
+# Show the value stored in density
+### Section 1.5 
+# Assign 42 to numeric_var
+# Assign "Dipodomys merriami" to species_name
+# Assign TRUE to is_mammal Remember: Case matters
+# Check class of numeric_var
+# Check class of species_name
+# Check class of is_mammal
