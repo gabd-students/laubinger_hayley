befe3daac51839e47b78d2601ad32613f6517e2d
Halaubi <hmlaubinger1s@semo.edu>
2020-07-03 21:19:18 -0500

    Finished Part 1,fixed version

--- Hw02/hw02-1.R
+++ Hw02/hw02-1.R
@@ -1,29 +1,55 @@
-# Hayley Laubinger
-# HW02 Part 1: Intro to Basics
-# Calculate 2 + 5
+# Add 30 and 12
+30 + 12
 ### Section 1.2
 # Division
-# Exponentiation
-#Modulo
+121/1.1
+# Exponentation
+2^5
+# Module
+28 %% 6
+#
 # Exponentiation without parentheses
+49^1/2
 # Exponentiation with parentheses
-### Section 1.3
+49^(1/2)
+#
+### Section 1.3 Variable Assignment
 # Assign the value 42 to x
+x <- 42
 # Print out the value of the variable x
+x
+# 
 # Assign the value 24 the variable d_merriami
+d_merriami <- 24
 # Assign the value 9 to d_ordii
+d_ordii <- 9
 # Check the results
+d_merriami 
+d_ordii
+#
 ### 1.4 Working with variables
-# Correcting the mistake
 # Add together the number of individuals of each species. 
-# Store the result in 'total_k_rats'.
+# Store the result in 'total_k_rats'
+total_k_rats <- d_merriami + d_ordii
 # Assign 1000 to a variable called 'area_sampled'
+area_sampled <- 1000
 # Divide 'total_k_rats' by 'area_sampled' to calculate 'density'. 
+density <- total_k_rats/area_sampled
 # Show the value stored in density
-### Section 1.5 
+0.038
+#
+### 1.5 Basic Data Types in R
 # Assign 42 to numeric_var
+numeric_var <- 42
 # Assign "Dipodomys merriami" to species_name
+species_name <- "Dipodomys merriami"
 # Assign TRUE to is_mammal Remember: Case matters
+is_mammal <- TRUE
+#
+### 1.6 What is the data Type?
 # Check class of numeric_var
+class(numeric_var)
 # Check class of species_name
+class(species_name)
 # Check class of is_mammal
+class(is_mammal)
